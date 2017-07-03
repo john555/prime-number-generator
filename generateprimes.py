5e836bebf77518d8719db8e1572c964d1f840093
John O. Paul <oyomthojohnpaul@gmail.com>
2017-07-03 16:18:36 +0300

    pass all tests
    minor changes inlude:
    - rename assertEquals to asserEqual

--- generateprimes.py
+++ generateprimes.py
@@ -3,8 +3,24 @@ import unittest
 def generate_primes(n):
     """generates prime numbers from 0 to n"""
     
-    if type(n) != type(0) or type(n) != type(0.1):
+    if type(n) != type(0) and type(n) != type(0.1):
         raise ValueError("invalid input, expecting a number")
+    
+    primes = []
+
+    for i in range(2, n+1):
+
+        divisions = 0
+
+        for j in range(2, i):
+
+            if i % j == 0:
+                divisions +=1
+
+        if divisions == 0:
+            primes.append(i)
+
+    return primes
 
 class GeneratePrimeTest(unittest.TestCase):
 
@@ -19,10 +35,10 @@ class GeneratePrimeTest(unittest.TestCase):
         self.assertEqual(type(generate_primes(0)), type([]))
 
     def test_returns_empty_list_as_output_for_1(self):
-        self.assertEquals(generate_primes(1), [])
+        self.assertEqual(generate_primes(1), [])
     
     def test_returns_correct_output_for_2(self):
-        self.assertEquals(generate_primes(2), [2])
+        self.assertEqual(generate_primes(2), [2])
     
     def test_returns_correct_output_for_13(self):
-        self.assertEquals(generate_primes(13), [2, 3, 5, 7, 11, 13])
+        self.assertEqual(generate_primes(13), [2, 3, 5, 7, 11, 13])
